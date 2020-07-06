1fdf3215edee72b21fc72cb0b7bb24090221aed3
fumiaki49 <me.fk39@gmail.com>
2020-07-06 17:31:28 +0900

    記述の修正と削除機能の追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
   var taskArray = [];
   var storageKey = 'savedTask';
+  var storageData = JSON.parse(localStorage.getItem(storageKey));
 
   readStorage();
 
@@ -13,9 +14,24 @@ $(function () {
     saveTask(taskData, deadlineData, priorityData);
   });
 
+  $(document).on('click', '.delete-btn', function() {
+    var getTaskCard = $(this).closest('.task-card');
+    var taskCardIndex = getTaskCard.index();
+    getTaskCard.remove();
+    taskArray.splice(taskCardIndex, 1);
+    saveStorage();
+    if(taskArray.length === 0) {
+      localStorage.clear();
+      showMessage();
+    }
+  })
+
   $('.clean-all-task').on('click', function() {
+    //task-listの子要素削除、localStorage、taskArrayのデータを初期化
     $('.tasks-list').empty();
     localStorage.clear();
+    taskArray = [];
+    showMessage();
   });
 
   function saveTask(receivedTask, receivedDeadline, receivedPriority) {
@@ -25,17 +41,29 @@ $(function () {
       priority: receivedPriority
     };
     taskArray.push(taskObject);
+    saveStorage();
+  };
+
+  function saveStorage() {
     localStorage.setItem(storageKey, JSON.stringify(taskArray));
   };
 
   function readStorage() {
-    var storageData = JSON.parse(localStorage.getItem(storageKey));
-    taskArray=storageData;
-    $.each(storageData, function(index, data) {
-      addList(data.task, data.deadline, data.priority);
-    });
+    if(storageData === null) {
+      showMessage();
+    } else {
+      taskArray=storageData; //ページを読み込んだ際に、既にlocalStorageにデータがあればtaskArrayに代入
+      $.each(storageData, function(index, data) {
+        addList(data.task, data.deadline, data.priority);
+      });
+    }
   };
-  
+
+  function showMessage() {
+    var message = `<li class ="message is-align-center">フォームから情報を入力してタスクを管理しましょう！</li>`
+    $('.tasks-list').append(message);
+  };
+
   function addList(receivedTask, receivedDeadline, receivedPriority) {
     var taskCardTemplate = `<li class="task-card">
                               <div class="task-name card-inner">${receivedTask}</div>
@@ -49,14 +77,17 @@ $(function () {
                                   </div>
                                 </div>
                                 <div class="task-detail__btn-box">
-                                  <label class="task-detail__btn-box__compleate-btn">
+                                  <label class="compleate-btn">
                                     <input type="checkbox">
                                     <span>完了</span>
                                   </label>
-                                  <button class="task-detail__btn-box__edit-task default-btn">編集</button>
+                                  <button class="delete-btn default-btn">
+                                    <i class="fas fa-trash"></i>
+                                  </button>
                                 </div>
                               </div>
                             </li>`
+    $('.message').remove();
     $('.tasks-list').append(taskCardTemplate);
     $('#task').val('');
     $('#deadline').val('');
